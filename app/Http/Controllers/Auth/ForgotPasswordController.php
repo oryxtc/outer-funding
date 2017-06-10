288f185172909dcd25327757031c132c7b8b2c36
oryxtc <oryxtc@gmail.com>
2017-06-10 13:07:17 +0800

    complete get password

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\ValidatorController;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
 {
@@ -28,5 +31,47 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+
+    public function resetPassword(Request $request){
+        $request_data=$request->all();
+        //验证数据
+        $this->validator($request_data)->validate();
+        //更新用户密码
+        $update_result=\DB::table('users')
+            ->where(['phone'=>$request_data['phone']])
+            ->update(['password'=>bcrypt($request_data['password'])]);
+        if ($update_result===false){
+            return response()->json(['message'=>'重置密码失败!'],303);
+        }
+        return response()->json(['message'=>'重置密码成功!']);
+    }
+
+    /**
+     * 验证验证码
+     * @param array $data
+     * @return mixed
+     */
+    protected function validator(array $data)
+    {
+        //自定义错误信息
+        $message = ['password.confirmed' => '两次密码不一致!'];
+
+        //验证数据类型
+        $validator = Validator::make($data, [
+            'phone'      => 'required|numeric',
+            'phone_code' => 'required|string',
+            'password'   => 'required|string|min:6|max:16|confirmed',
+        ], $message);
+
+        //验证手机注册验证码
+        $validator->after(function ($validator) use ($data) {
+            $validator_code_result = ValidatorController::validatorCode($data['phone'], $data['phone_code'], 'reset_pass');
+            if ($validator_code_result === false) {
+                $validator->errors()->add('phone_code', '验证码错误!');
+            }
+        });
+        return $validator;
     }
 }
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -17,24 +17,36 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('password.request') }}">
                         {{ csrf_field() }}
 
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">手机号</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
+                                <input id="name" type="number" class="form-control" name="phone" value="{{ old('phone') }}" required autofocus>
 
-                                @if ($errors->has('email'))
+                                @if ($errors->has('phone'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('phone_code') ? ' has-error' : '' }}">
+                            <label for="phone_code" class="col-md-4 control-label">手机验证</label>
+
+                            <div class="col-md-6">
+                                <input id="phone_code" type="text" class="form-control" name="phone_code"  required>
+
+                                @if ($errors->has('phone_code'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone_code') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">新密码</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -47,23 +59,18 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">确认密码</label>
+
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                @if ($errors->has('password_confirmation'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                @endif
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Reset Password
+                                    确认重置
                                 </button>
                             </div>
                         </div>
