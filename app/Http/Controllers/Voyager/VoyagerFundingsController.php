e786f63d26075f3099a7641d8554bcc166358efb
oryxtc <oryxtc@gmail.com>
2017-06-14 14:05:56 +0800

    配资 列表

--- app/Http/Controllers/Voyager/VoyagerFundingsController.php
+++ app/Http/Controllers/Voyager/VoyagerFundingsController.php
@@ -5,9 +5,67 @@ namespace App\Http\Controllers\Voyager;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\Traits\BreadRelationshipParser;
+
 
 class VoyagerFundingsController extends Controller
 {
+    use BreadRelationshipParser;
+
+    public function index(Request $request)
+    {
+        // GET THE SLUG, ex. 'posts', 'pages', etc.
+        $slug = $this->getSlug($request);
+
+        // GET THE DataType based on the slug
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Check permission
+        Voyager::canOrFail('browse_' . $dataType->name);
+
+        $getter = $dataType->server_side ? 'paginate' : 'get';
+
+        // Next Get or Paginate the actual content from the MODEL that corresponds to the slug DataType
+        if (strlen($dataType->model_name) != 0) {
+            $model = app($dataType->model_name);
+
+            $relationships = $this->getRelationships($dataType);
+            if ($model->timestamps) {
+                $status=$request->get('status');
+                if(isset($status)){
+                    $dataTypeContent = call_user_func([$model->with($relationships)->where([['status',$status]])->latest(), $getter]);
+                }else{
+                    $dataTypeContent = call_user_func([$model->with($relationships)->latest(), $getter]);
+                }
+            } else {
+                $dataTypeContent = call_user_func([
+                    $model->with($relationships)->orderBy($model->getKeyName(), 'DESC'),
+                    $getter,
+                ]);
+            }
+
+            //Replace relationships' keys for labels and create READ links if a slug is provided.
+            $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType);
+        } else {
+            // If Model doesn't exist, get data from table name
+            $dataTypeContent = call_user_func([DB::table($dataType->name), $getter]);
+            $model           = false;
+        }
+
+        // Check if BREAD is Translatable
+        $isModelTranslatable = is_bread_translatable($model);
+
+        $view = 'voyager::bread.browse';
+
+        if (view()->exists("voyager::$slug.browse")) {
+            $view = "voyager::$slug.browse";
+        }
+
+        return view($view, compact('dataType', 'dataTypeContent', 'isModelTranslatable'));
+    }
+
     /**
      * 审核配资
      * @param Request $request
@@ -45,4 +103,6 @@ class VoyagerFundingsController extends Controller
                 'alert-type' => 'success',
             ]);
     }
+
+
 }
