2a0602088e45bb851c76b46b2b586c3f9a2e76fb
oryxtc <oryxtc@gmail.com>
2017-06-21 14:43:20 +0800

    修复

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -94,6 +94,11 @@ class UsersController extends Controller
             if ($validator->fails()) {
             return redirect('/yyyouyu');
         }
+        //验证是否实名认证
+        $user_info = \DB::table('users')
+            ->select('actual_name', 'id_card', 'phone')
+            ->where('id', \Auth::id())
+            ->first();
         $date = date('Y-m-d H:i:s', time());
         //准备数据
         $data = $this->fundingConfig($A, $B, $Y);
